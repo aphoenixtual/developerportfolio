4a6288c453472606f0093887b3d68365a80cc761
Aayush Patel <ayushp264@gmail.com>
2025-04-14 15:05:16 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -35,7 +35,7 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
     datetime: new Date().toString()
   }).then(() => {
     const statusEl = document.getElementById('status');
-    statusEl.innerHTML = `<p>Hello, <strong>${name}</strong>. Thank you for contacting me.<br>I will contact you soon.</p>`;
+    statusEl.innerHTML = `<p>Hey, <strong>${name}</strong>. <br>Thank you for connecting.<br>I will contact you soon.</p>`;
     statusEl.classList.add("show");
     document.getElementById('contactForm').reset();
   }).catch(() => {
