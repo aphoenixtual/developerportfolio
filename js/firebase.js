26ec7f98e20bb1ee64b91fb95edcae2dc90baa53
Aayush Patel <ayushp264@gmail.com>
2025-04-14 13:44:12 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,11 +6,11 @@ import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/
 const firebaseConfig = {
   apiKey: "AIzaSyAdsT5uy07GzLjswxhP6SvH_qT5OCFXjRU",
   authDomain: "contactformwebsite-b2c9d.firebaseapp.com",
+  databaseURL: "https://contactformwebsite-b2c9d-default-rtdb.firebaseio.com",
   projectId: "contactformwebsite-b2c9d",
   storageBucket: "contactformwebsite-b2c9d.firebasestorage.app",
   messagingSenderId: "701514212296",
   appId: "1:701514212296:web:f4842686530bfea0c056fd"
-  databaseURL: "https://contactformwebsite-b2c9d-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
