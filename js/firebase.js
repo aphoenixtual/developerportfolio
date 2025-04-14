74b06dfc9bc2fd3cf13a8a07c29216505ec26f0a
Aayush Patel <ayushp264@gmail.com>
2025-04-14 13:33:07 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,64 +1,44 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAdsT5uy07GzLjswxhP6SvH_qT5OCFXjRU",
-    authDomain: "contactformwebsite-b2c9d.firebaseapp.com",
-    projectId: "contactformwebsite-b2c9d",
-    storageBucket: "contactformwebsite-b2c9d.firebasestorage.app",
-    messagingSenderId: "701514212296",
-    appId: "1:701514212296:web:f4842686530bfea0c056fd"
-  };
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 
+// Tera config
+const firebaseConfig = {
+  apiKey: "AIzaSyAdsT5uy07GzLjswxhP6SvH_qT5OCFXjRU",
+  authDomain: "contactformwebsite-b2c9d.firebaseapp.com",
+  projectId: "contactformwebsite-b2c9d",
+  storageBucket: "contactformwebsite-b2c9d.firebasestorage.app",
+  messagingSenderId: "701514212296",
+  appId: "1:701514212296:web:f4842686530bfea0c056fd"
+};
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-var messagesRef = firebase.database().ref('ContactDetails/');
+// 👇 contact form logic
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-document.getElementById('contactForm').addEventListener('submit', submitForm);
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
 
-function submitForm(e) {
-    e.preventDefault();
+  const messagesRef = ref(database, 'ContactDetails/');
+  const newMessageRef = push(messagesRef);
 
-    // Get values 
-    var name = getInputVal('name');
-    var email = getInputVal('email');
-    var message = getInputVal('message');
-
-    saveMessage(name, email, message);
-}
-
-// Function to get get form values 
-function getInputVal(id) {
-    return document.getElementById(id).value;
-}
-function setInputVal(id) {
-    // return document.getElementById(id) = 
-}
-
-// Save message to firebase 
-function saveMessage(name, email, message) {
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-        name: name,
-        email: email,
-        message: message,
-        datetime: new Date().toString()
-    }).then(() => {
-        document.getElementById('status').innerHTML =
-            " <p style='font-size:18px; font-weight:500; '> Hello," + name + " .Thank you for contacting me. <br> I will contact you soon. </p>"
-        document.getElementById('status').style.color = "green";
-        document.getElementById('status').style.fontWeight = "500";
-
-        setInputVal('name');
-        setInputVal('email');
-        setInputVal('message');
-
-    }
-
-    ).catch(() => {
-        document.getElementById('status').innerHTML =
-            "There is some issue !! Please Try again"
-        document.getElementById('status').style.color = "red";
-        document.getElementById('status').style.fontWeight = "500";
-        document.getElementById('status').style.fontSize = "20";
-    }
-    );
-}
+  set(newMessageRef, {
+    name: name,
+    email: email,
+    message: message,
+    datetime: new Date().toString()
+  }).then(() => {
+    document.getElementById('status').innerHTML =
+      `<p style='font-size:18px; font-weight:500;'> Hello, ${name}. Thank you for contacting me. <br> I will contact you soon. </p>`;
+    document.getElementById('status').style.color = "green";
+    document.getElementById('contactForm').reset();
+  }).catch(() => {
+    document.getElementById('status').innerText = "There is some issue!! Please Try again";
+    document.getElementById('status').style.color = "red";
+  });
+});
